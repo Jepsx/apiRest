61ab19927a518c91d94566001d23887f1fbcea90
Julian Pinzon <julianchos1998@gmail.com>
2022-05-18 16:50:38 -0500

    refreshDone

--- js/main.js
+++ js/main.js
@@ -4,13 +4,32 @@ const URL = 'https://api.thedogapi.com/v1/images/search?limit=3';
 const changeImage = async ()=>{
     let uldData = await fetch(URL);
     let json = await uldData.json();
-    let breeds = json[0].breeds;
     const exploreSection = document.querySelector('#explore');
-    debugger;
-    // const imageCap = document.querySelector('figcaption');
-    imageSrc.src = json[0].url;
-    // imageSrc.alt = breeds[0].bred_for;
-    // imageCap.innerText = breeds[0].bred_for;
+    json.forEach((item)=>{
+        if(exploreSection.children.length == 4){
+            let expChildren = exploreSection.children;
+            for(i = 0; i < expChildren.length; i++){
+                let expChild = expChildren[i];
+                console.log(expChild.localName);
+                if(expChild.localName === 'articule'){
+                    expChild.remove();
+                }
+             }
+
+        }else{
+            const art = document.createElement("articule");
+            const pic = document.createElement("picture");
+            const img = document.createElement('img');
+            pic.classList.add('img-container');
+            img.classList.add('img-container__img');
+            img.src = item.url;
+            art.append(pic);
+            pic.append(img);
+            exploreSection.insertAdjacentElement('afterbegin',art);
+        }
+
+    });
+
 }
 
 changeImage();
