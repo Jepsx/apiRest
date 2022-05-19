9dfbef68abb6cc9bd19450d7abc8e3ee7aad0bb0
Julian Pinzon <julianchos1998@gmail.com>
2022-05-19 11:30:40 -0500

    css&js

--- js/main.js
+++ js/main.js
@@ -1,39 +1,81 @@
-const URL = 'https://api.thedogapi.com/v1/images/search?limit=3';
+const URL_EXPLORE_RANDOM_DOGS = 'https://api.thedogapi.com/v1/images/search?limit=3';
+const URL_FAVORITES_DOGS = 'https://api.thedogapi.com/v1/favourites';
 
 
 const changeImage = async ()=>{
-    let uldData = await fetch(URL);
-    let json = await uldData.json();
     const exploreSection = document.querySelector('#explore');
-    json.forEach((item)=>{
-        if(exploreSection.children.length == 4){
-            let expChildren = exploreSection.children;
-            for(i = expChildren.length - 1; i >=0; i--){
-                let expChild = expChildren[i];
+    let uldData = await fetch(URL_EXPLORE_RANDOM_DOGS);
+    if(uldData.status != 200){
+        const art = document.createElement("articule");     
+        const span = document.createElement("span");     
+        const p = document.createElement("p");
+        span.innerText = uldData.status;
+        p.innerText = 'Oops!, something went wrong.'
+        art.classList.add('error');
+        span.classList.add('errorSpan');
+        p.classList.add('errorP');
+        art.append(span);
+        art.append(p);
+        exploreSection.append(art);
 
-                if(expChild.localName === 'articule'){
-                    expChild.remove();
-                }
-             }
+    }else{
 
-        }
-            const art = document.createElement("articule");
-            const pic = document.createElement("picture");
-            const img = document.createElement('img');
-            art.classList.add('explore__article')
-            pic.classList.add('img-container');
-            img.classList.add('img-container__img');
-            img.src = item.url;
-            art.append(pic);
-            pic.append(img);
-            exploreSection.insertAdjacentElement('afterbegin',art);
-
-
-    });
+        let json = await uldData.json();
+    
+        json.forEach((item)=>{
+            if(exploreSection.children.length == 5){
+                let expChildren = exploreSection.children;
+                for(i = expChildren.length - 1; i >=0; i--){
+                    let expChild = expChildren[i];
+    
+                    if(expChild.localName === 'articule'){
+                        expChild.remove();
+                    }
+                 }
+    
+            }
+                const art = document.createElement("articule");
+                const pic = document.createElement("picture");
+                const img = document.createElement('img');
+                art.classList.add('explore__article');
+                pic.classList.add('img-container');
+                img.classList.add('img-container__img');
+                img.src = item.url;
+                art.append(pic);
+                pic.append(img);
+                exploreSection.insertAdjacentElement('afterbegin',art);
+    
+    
+        });
+    }
 
 }
 
+const getFavorites = async ()=>{
+    const favoriteSection = document.querySelector('#favorites');
+    let uldData = await fetch(URL_FAVORITES_DOGS);
+    console.log(uldData);
+
+    if(uldData.status != 200){
+        const art = document.createElement("articule");     
+        const span = document.createElement("span");     
+        const p = document.createElement("p");
+        span.innerText = uldData.status;
+        p.innerText = 'Oops!, something went wrong.'
+        art.classList.add('error');
+        span.classList.add('errorSpan');
+        p.classList.add('errorP');
+        art.append(span);
+        art.append(p);
+        favoriteSection.append(art);
+
+    }else{
+        let json = await uldData.json();
+    }
+}
+
 changeImage();
+getFavorites();
 
 const btn = document.querySelector('button');
 btn.addEventListener('click', changeImage);
